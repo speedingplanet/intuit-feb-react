582c295164eac1ad95b3c1a76e1be6c5726e7d9f
John Paxton <johnpaxton@gmail.com>
2023-01-12 10:08:13 -0500

    End of Day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import CounterState from './demos/CounterState';
+import EventHandling from './demos/EventHandling';
 import Footer from './Footer';
+import GridHeaderRow from './labs/GridHeaderRow';
 
 function App() {
+  // let columnNames = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Iota'];
+  let columnNames = ['First Name', 'Last Name', 'City', 'State'];
+
   return (
     <main className="container">
       <header>
         <h1>Some terrific app</h1>
         <hr />
       </header>
+      {/* <GridHeaderRow columnNames={columnNames} /> */}
+      {/* <EventHandling /> */}
+      <CounterState />
       <Footer companyName="Neat-o Partners" year={2022} />
     </main>
   );
