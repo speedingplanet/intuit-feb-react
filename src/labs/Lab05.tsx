d07dc61160b08e60e20da72d51e48e690e255887
John Paxton <johnpaxton@gmail.com>
2023-01-13 10:52:04 -0500

    Solution for lab 5

--- src/labs/Lab05.tsx
+++ src/labs/Lab05.tsx
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
 import './data-grid.css';
+import { people } from '../data/people';
 
 export default function Lab05() {
   let columns = ['First Name', 'Last Name', 'City', 'State'];
   return (
     <section>
       <h3>Lab 5: Iterative content</h3>
-      <GridHeaderRow columnNames={columns} />
+      <div className="grid-container">
+        <GridHeaderRow columnNames={columns} />
+        <GridBody people={people} />
+      </div>
     </section>
+  );
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  email: string;
+  phoneNumber: string;
+  city: string;
+  province: string | null;
+  country: string;
+  postalCode: string;
+  id: number;
+}
+
+interface GridBodyProps {
+  people: Person[];
+}
+
+type PersonFields = keyof Person;
+
+function GridBody({ people }: GridBodyProps) {
+  let fields: PersonFields[] = ['firstName', 'lastName', 'city', 'province'];
+  return (
+    <div className="grid grid-body">
+      {people.map((person) => (
+        <div className="grid-body-row" key={person.id}>
+          {fields.map((field) => (
+            <div className="grid-body-cell" key={field}>
+              {person[field]}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
@@ -48,7 +88,7 @@ function GridHeaderRow({ columnNames }: GridHeaderRowProps) {
     );
   });
 
-  return <div className="grid-header-row">{headers}</div>;
+  return <div className="grid grid-header-row">{headers}</div>;
 }
 
 interface GridHeaderProps {
