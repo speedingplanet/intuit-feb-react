0f61483809879a930b9a106154c8e13b52b4184a
John Paxton <johnpaxton@gmail.com>
2023-01-13 13:01:40 -0500

    Starter for type discussion

---
+++ src/demos/types-guide.ts
@@ -0,0 +1,33 @@
+// Implicit types
+
+// Explicit types
+// in-line types
+
+// Arrays
+// Objects
+// Functions
+
+// Type definitions
+// Interface
+// Type
+// Class
+// Enum
+
+// Union types
+
+// Intersection types
+
+// Extending types
+// Interface/class with extends
+// Type with union or intersection operators
+
+// Utilities
+// keyof
+// typeof
+// Pick / Omit
+// Partial / Required
+// Readonly
+
+// Generics
+
+export {};
