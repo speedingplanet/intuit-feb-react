862261a02a28f1646fad02c2c54c5f749ff7e88e
John Paxton <johnpaxton@gmail.com>
2023-01-23 18:00:10 -0500

    Class vs functional components

---
+++ src/demos/class-vs-functional/ClassComponent.tsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+/*
+ * Three areas to compare between functional and class-based components:
+ * - event handling
+ * - state
+ * - lifecycle
+ *
+ */
+
+export default class ClassComponent extends Component<any, { counter: number }> {
+  counter = 0;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  // useEffect(() => void; [])
+  componentDidMount(): void {
+    console.log('componentDidMount: The component has been mounted into the DOM');
+  }
+
+  // useEffect(() => void)
+  componentDidUpdate(
+    prevProps: Readonly<any>,
+    prevState: Readonly<{ counter: number }>,
+    snapshot?: any
+  ): void {
+    console.log('componentDidUpdate: The component has updated for some reason.');
+  }
+
+  // useEffect(() => { return componentWillUnmount})
+  componentWillUnmount(): void {
+    console.log('componentWillUmount: The ClassComponent is about to unmount');
+  }
+
+  // Explicit render method for returning information
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <button className="btn btn-danger" onClick={this.handleClick}>
+            Class-based Click
+          </button>
+          &nbsp;<span>{this.state.counter}</span>
+        </div>
+      </div>
+    );
+  }
+}
