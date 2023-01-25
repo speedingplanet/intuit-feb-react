e0e1ad8e98cdd1a08119d5ebb41f56b7c6bd876d
John Paxton <johnpaxton@gmail.com>
2023-01-25 13:46:30 -0500

    Eliminate warning

--- src/demos/FormInputs.tsx
+++ src/demos/FormInputs.tsx
@@ -40,7 +40,7 @@ export default function FormInputs() {
             id="ex-select"
             value={selectInput}
             onChange={(e) => setSelectInput(e.currentTarget.value)}>
-            <option selected>Open this select menu</option>
+            <option>Open this select menu</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
