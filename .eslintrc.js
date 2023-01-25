7539f97209108fea1c84ca771b27941a706091a6
John Paxton <johnpaxton@gmail.com>
2023-01-25 10:16:11 -0500

    Fix issue with switch-case

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
       'error',
       { functions: 'never', arrays: 'always-multiline', objects: 'always-multiline' },
     ],
-    indent: ['error', 2, { MemberExpression: 1 }],
+    indent: ['error', 2, { MemberExpression: 1, SwitchCase: 1 }],
     'max-len': ['warn', { code: 100, comments: 120 }],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
